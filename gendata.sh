ab8dbeaf8b7cd6fb791eed779259a6807907b9e0
iambernie <iambernie@localhost>
2013-03-25 12:41:28 +0100

    examples

---
+++ gendata.sh
@@ -0,0 +1,7 @@
+#amuse hydro_fi.py -N 4000 -n 100 -t 1.0 -P evolveN4000n100t1.hdf5
+#amuse hydro_fi.py -N 2000 -n 100 -t 1.0 -P evolveN2000n100t1.hdf5
+#amuse hydro_fi.py -N 1000 -n 100 -t 1.0 -P evolveN1000n100t1.hdf5
+
+amuse hydro_fi.py -N 2000 -n 300 -t 0.5 -P evolveN2000n300t0.5.hdf5
+#amuse hydro_fi.py -n 200 -t 0.5 -P smash_vx20vy3.hdf5 -p body_N1000n50_1.hdf5 -q body_N1000n50_2.hdf5 --vx 20 --vy 0 --vz 0 
+
---
+++ genmovies.sh
@@ -0,0 +1,10 @@
+amuse animation.py -f smash_vx30.hdf5 -r 8
+amuse animation.py -f smash_vx40.hdf5 -r 8
+amuse animation.py -f smash_vx50.hdf5 -r 8
+amuse animation.py -f smash_vx60.hdf5 -r 8
+amuse animation.py -f smash_vx76.hdf5 -r 8
+
+amuse animation.py -f smash_vx10vy5.hdf5 -r 4
+amuse animation.py -f smash_vx10vy5.hdf5 -r 8
+amuse animation.py -f smash_vx10vy5.hdf5 -r 16
+amuse animation.py -f smash_vx10vy5.hdf5 -r 32
