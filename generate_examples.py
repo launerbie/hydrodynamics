5b4a649703750b9890615c1adb466e57a41a3a8e
iambernie <iambernie@localhost>
2013-03-30 16:13:13 +0100

    generate more examples

--- generate_examples.py
+++ generate_examples.py
@@ -19,8 +19,8 @@ def main(options):
     
 def evolve_plummer(daemon=False):
     if daemon == True:
-#        command = 'amuse evolve.py -N 2000 -n 300 -t 0.5 -H evolveN2000n300t0.5.hdf5 -B bodyN2000n300.hdf5'
-#        subprocess.call(command, shell=True)
+        command = 'amuse evolve.py -N 2000 -n 300 -t 0.5 -H evolveN2000n300t0.5.hdf5 -B bodyN2000n300.hdf5'
+        subprocess.call(command, shell=True)
         command = 'amuse evolve.py -N 1000 -n 100 -t 0.5 -H evolveN1000n100t0.5.hdf5 -B bodyN1000n100.hdf5'
         subprocess.call(command, shell=True)
     else:
@@ -31,14 +31,14 @@ def evolve_plummer(daemon=False):
 
 def smash_plummers(daemon=False):
     if daemon == True:
-#        command = 'amuse evolve.py -n 300 -t 0.5 -H no_velocity.hdf5 -p bodies/bodyN1000n100.hdf5 -q bodies/bodyN1000n100.hdf5'
-#        subprocess.call(command, shell=True)
-#        command = 'amuse evolve.py -n 200 -t 1.5 -H smash_vx20.hdf5 -p bodies/bodyN1000n100.hdf5 -q bodies/bodyN1000n100.hdf5 --vx 20'
-#        subprocess.call(command, shell=True)
-#        command = 'amuse evolve.py -n 200 -t 0.5 1 -H smash_vx76.hdf5 -p bodies/bodyN1000n100.hdf5 -q bodies/bodyN1000n100.hdf5 --vx 76'
-#        subprocess.call(command, shell=True)
-#        command = 'amuse evolve.py -n 200 -t 1.5 1 -H smash_vx10vy5 -p bodies/bodyN1000n100.hdf5 -q bodies/bodyN1000n100.hdf5 --vx 10 --vy 5'
-#        subprocess.call(command, shell=True)
+        command = 'amuse evolve.py -n 300 -t 0.5 -H no_velocity.hdf5 -p bodies/bodyN1000n100.hdf5 -q bodies/bodyN1000n100.hdf5'
+        subprocess.call(command, shell=True)
+        command = 'amuse evolve.py -n 200 -t 1.5 -H smash_vx20.hdf5 -p bodies/bodyN1000n100.hdf5 -q bodies/bodyN1000n100.hdf5 --vx 20'
+        subprocess.call(command, shell=True)
+        command = 'amuse evolve.py -n 200 -t 0.5 1 -H smash_vx76.hdf5 -p bodies/bodyN1000n100.hdf5 -q bodies/bodyN1000n100.hdf5 --vx 76'
+        subprocess.call(command, shell=True)
+        command = 'amuse evolve.py -n 200 -t 1.5 1 -H smash_vx10vy5 -p bodies/bodyN1000n100.hdf5 -q bodies/bodyN1000n100.hdf5 --vx 10 --vy 5'
+        subprocess.call(command, shell=True)
         command = 'amuse evolve.py -n 200 -t 1.5 1 -H smash_vx10vy10 -p bodies/bodyN1000n100.hdf5 -q bodies/bodyN1000n100.hdf5 --vx 10 --vy 10'
         subprocess.call(command, shell=True)
     else:
@@ -55,16 +55,16 @@ def smash_plummers(daemon=False):
 
 def create_animation(daemon=False):
     if daemon == True:
-#        command = 'amuse animation.py -f hydroresults/evolveN2000n300t0.5.hdf5 -r 2'
-#        subprocess.call(command, shell=True)
-#        command = 'amuse animation.py -f hydroresults/no_velocity.hdf5 -r 3'
-#        subprocess.call(command, shell=True)
-#        command = 'amuse animation.py -f hydroresults/smash_vx20.hdf5 -r 4'
-#        subprocess.call(command, shell=True)
-#        command = 'amuse animation.py -f hydroresults/smash_vx76.hdf5 -r 8'
-#        subprocess.call(command, shell=True)
-#        command = 'amuse animation.py -f hydroresults/smash_vx10vy5 -r 4'
-#        subprocess.call(command, shell=True)
+        command = 'amuse animation.py -f hydroresults/evolveN2000n300t0.5.hdf5 -r 2'
+        subprocess.call(command, shell=True)
+        command = 'amuse animation.py -f hydroresults/no_velocity.hdf5 -r 3'
+        subprocess.call(command, shell=True)
+        command = 'amuse animation.py -f hydroresults/smash_vx20.hdf5 -r 4'
+        subprocess.call(command, shell=True)
+        command = 'amuse animation.py -f hydroresults/smash_vx76.hdf5 -r 8'
+        subprocess.call(command, shell=True)
+        command = 'amuse animation.py -f hydroresults/smash_vx10vy5 -r 4'
+        subprocess.call(command, shell=True)
         command = 'amuse animation.py -f hydroresults/smash_vx10vy10 -r 4'
         subprocess.call(command, shell=True)
     else:
